18df96eff837c5fcb1c737fa12a0d77c9dca394d
Emanuel Tenner <lehnertgkk@gmail.com>
2017-07-19 09:53:12 +0000

    Prepared Email Notification Functionality

---
+++ Changeling/mailer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changeling
+{
+    class mailer
+    {
+        public mailer()
+        {
+        }
+
+        private void smtp_mailer(string address, string password)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("address");
+                mail.To.Add("thomas.lehner@cortado.com");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("mail Send");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
